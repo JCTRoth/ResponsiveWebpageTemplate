56288743d1b15e0731f760012236380082bc90b9
JCTRoth <jonasroth@mailbase.info>
2019-10-23 17:01:57 +0200

    Init.

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+/* Java Script way of doing DOM stuff */
+/*
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+} */
+
+
+
+/* JQuery to Hide DOM Entery */
+function makeWheelInvis(){
+/* $("#wheel1").css("visibility", "hidden"); */
+}
+
+/* Delay Timer if needed */
+function animation(){
+
+/* $("#play").hide();
+$("#wheel").animate({"left":"500px"}, "slow").fadeOut(2000);
+$("#logo").delay(1950).animate({left: '250px'}, "slow").fadeIn(3000);
+$(".button").delay(3600).fadeIn(7000); */
+
+}
+
+/* Some JQuery Code to Change DOM */
+/*
+$('body').on('click','img',function(){animation();});
+$('body').on('click','.button',function(){$(".button").hide();
+$("#text").fadeIn(2000);}); */
